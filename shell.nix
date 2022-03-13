414845b2f599bfbff3380279eb396fc467f5044b
Michael Wilding <heymichaelwilding@gmail.com>
2022-03-12 17:28:29 -0800

    mvp up to https://www.howtographql.com/graphql-elixir/2-queries/

---
+++ shell.nix
@@ -0,0 +1,62 @@
+
+{ mypkgs ? import <nixpkgs> {} }:
+
+with mypkgs;
+
+let
+  inherit (lib) optional optionals;
+
+  elixir = elixir_1_13;
+  postgresql = postgresql_10;
+in
+
+mkShell {
+  buildInputs = [
+  elixir
+  nodejs
+  git
+  postgresql
+]
+    ++ optional stdenv.isLinux inotify-tools # For file_system on Linux.
+    ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+      # For file_system on macOS.
+      CoreFoundation
+      CoreServices
+    ]);
+
+    # Put the PostgreSQL databases in the project diretory.
+shellHook = ''
+
+# init the db with 
+# mkdir -p ./db/
+export PGDATA="$(pwd)/db"
+export PGHOST="$(pwd)"
+export PGPORT="5433"
+
+if [[ ! $(grep listen_address $PGDATA/postgresql.conf) ]]; then
+echo "db does not exist, creating "
+initdb -D $PGDATA --no-locale --encoding=UTF8
+
+cat >> "$PGDATA/postgresql.conf" <<-EOF
+listen_addresses = 'localhost'
+port = $PGPORT
+unix_socket_directories = '$PGHOST'
+EOF
+
+# ...create a database using the name Postgres defaults to.
+echo "CREATE USER postgres SUPERUSER;" | postgres --single -E postgres
+echo "CREATE DATABASE postgres WITH OWNER postgres;" | postgres --single -E postgres
+fi
+
+# postgres &
+
+## command to access the db after start
+# psql -h localhost postgres
+
+## command to kill the db 
+# pg_ctl -D ./db stop
+
+# trap "pg_ctl -D ./db stop" EXIT
+
+    '';
+}
