e0801ac6ac250dc199c88fd3b3e145d0899006e2
Michael Wilding <heymichaelwilding@gmail.com>
2022-03-13 19:12:20 -0700

    adds mutation for creating a new link WIP

--- lib/community/web/schema.ex
+++ lib/community/web/schema.ex
@@ -14,5 +14,13 @@ defmodule Community.Web.Schema do
     field :all_links, non_null(list_of(non_null(:link))) do
       resolve(&Web.NewsResolver.all_links/3)
     end
+
+    mutation do
+      @desc "create a new link"
+      field :create_link, :link do
+        arg :url, non_null(:string)
+
+        resolve &NewsResolver.create_link/3
+    end
   end
 end
